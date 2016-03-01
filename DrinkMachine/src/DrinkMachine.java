a338edc96b49af20d0c67470faec2352d2ab0791
Zachary Murphy <murphyz@my.wccc.edu>
2016-03-01 18:42:52 -0500

    panels done...maybe
    panels are done for the most part, now going to try to make
    calculations.

--- DrinkMachine/src/DrinkMachine.java
+++ DrinkMachine/src/DrinkMachine.java
@@ -5,7 +5,9 @@
  */
 
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import javax.swing.JApplet;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
@@ -26,7 +28,13 @@ public class DrinkMachine extends JApplet {
  
     public void init() {
         buildSodaOptionsPanel();
+        
+        buildInputPanel();
+        buildOutputPanel();
+        setLayout(new GridLayout(3,1));
+        add(input);
         add(sodaOptions);
+        add(output);
     }
 
     // TODO overwrite start(), stop() and destroy() methods
@@ -38,11 +46,11 @@ private void buildSodaOptionsPanel(){
     //making new label to give instructions
     JLabel sodaMessage = new JLabel ("Select from: ");
     //create radio buttons
-    JRadioButton cola = new JRadioButton("Cola");
-    JRadioButton lemonLime = new JRadioButton("Lemon Lime");
-    JRadioButton grape = new JRadioButton ("Grape Sode");
-    JRadioButton rootBeer = new JRadioButton ("Root beer");
-    JRadioButton bWater = new JRadioButton ("Bottled Water");
+    JButton cola = new JButton("Cola");
+    JButton lemonLime = new JButton("Lemon Lime");
+    JButton grape = new JButton ("Grape Sode");
+    JButton rootBeer = new JButton ("Root beer");
+    JButton bWater = new JButton ("Bottled Water");
     //layout manager for panel
     sodaOptions.setLayout(new FlowLayout(FlowLayout.RIGHT));
     
@@ -58,6 +66,29 @@ private void buildSodaOptionsPanel(){
 private void buildInputPanel(){
     input = new JPanel();
     JLabel inputMessage = new JLabel("Enter the amount tendered.");
+    tendered = new JTextField(15);
+    JButton enter = new JButton("Enter");
+    input.setLayout(new FlowLayout(FlowLayout.LEFT));
+    
+    input.add(inputMessage);
+    input.add(tendered);
+    input.add(enter);
+    
     
 }
+
+private void buildOutputPanel(){
+    output = new JPanel();
+    JTextField change = new JTextField(10);
+    change.setEditable(false);
+    JTextField status =  new JTextField(10);
+    status.setEditable(false);
+    
+    
+    
+    output.add(change);
+    output.add(status);
+    
+}
+
 }
