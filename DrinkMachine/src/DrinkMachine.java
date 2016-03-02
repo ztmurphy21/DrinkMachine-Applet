26fb30b588c9e2eabc04c73f48515c397b8df4ad
Zachary Murphy <murphyz@my.wccc.edu>
2016-03-01 21:04:34 -0500

    almost there...

--- DrinkMachine/src/DrinkMachine.java
+++ DrinkMachine/src/DrinkMachine.java
@@ -4,8 +4,11 @@
  * and open the template in the editor.
  */
 
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -22,19 +25,37 @@ import javax.swing.JTextField;
 public class DrinkMachine extends JApplet {
 
  private JPanel sodaOptions; // to hold the selectrion of sodea buttons
- private JPanel input; //panel where user will input everything
- private JPanel output; // panel where user will receive their change and if something is out of stock
+ private JPanel main; 
  private JTextField tendered;
+ private JTextField orderTotal;
+ private JTextField change;
+ private JTextField status;
+ private JButton cola;
+ private JButton lemonLime;
+ private JButton grape;
+ private JButton rootBeer;
+ private JButton bWater;
+ 
+ int avCola = 20;
+ int avLemonLime = 20;
+ int avGrape = 20;
+ int avRb = 20;
+ int avBWater = 20;
+ 
+double inputMoney = 0.0;
+ double changeGiven = 0.0;
+ double totalFunds = 0.0;
+ 
  
     public void init() {
         buildSodaOptionsPanel();
         
-        buildInputPanel();
-        buildOutputPanel();
-        setLayout(new GridLayout(3,1));
-        add(input);
-        add(sodaOptions);
-        add(output);
+        buildPanel();
+ 
+
+        add(main,BorderLayout.WEST);
+        add(sodaOptions,BorderLayout.EAST);
+
     }
 
     // TODO overwrite start(), stop() and destroy() methods
@@ -43,52 +64,155 @@ public class DrinkMachine extends JApplet {
 private void buildSodaOptionsPanel(){
     //declare panel
     sodaOptions = new JPanel();
+    sodaOptions.setLayout(new GridLayout(5,1));
     //making new label to give instructions
     JLabel sodaMessage = new JLabel ("Select from: ");
     //create radio buttons
-    JButton cola = new JButton("Cola");
-    JButton lemonLime = new JButton("Lemon Lime");
-    JButton grape = new JButton ("Grape Sode");
-    JButton rootBeer = new JButton ("Root beer");
-    JButton bWater = new JButton ("Bottled Water");
+     cola = new JButton("Cola");
+    lemonLime = new JButton("Lemon Lime");
+     grape = new JButton ("Grape Sode");
+     rootBeer = new JButton ("Root beer");
+     bWater = new JButton ("Bottled Water");
     //layout manager for panel
-    sodaOptions.setLayout(new FlowLayout(FlowLayout.RIGHT));
     
     //add everything
+    cola.addActionListener(new ColaListener());
     sodaOptions.add(sodaMessage);
     sodaOptions.add(cola);
+    lemonLime.addActionListener(new LemonListener());
     sodaOptions.add(lemonLime);
+    grape.addActionListener(new GrapeListener());
     sodaOptions.add(grape);
+    rootBeer.addActionListener(new RBListener());
     sodaOptions.add(rootBeer);
+    bWater.addActionListener(new BWaterListener());
     sodaOptions.add(bWater);
     
 }
-private void buildInputPanel(){
-    input = new JPanel();
+private void buildPanel(){
+    main = new JPanel();
     JLabel inputMessage = new JLabel("Enter the amount tendered.");
     tendered = new JTextField(15);
-    JButton enter = new JButton("Enter");
-    input.setLayout(new FlowLayout(FlowLayout.LEFT));
     
-    input.add(inputMessage);
-    input.add(tendered);
-    input.add(enter);
+    main.add(inputMessage);
+    main.add(tendered);
     
+    
+
+
+
+    JLabel changeLabel = new JLabel("Change: ");
+    change = new JTextField(10);
+    change.setEditable(false);
+    JLabel statusLabel = new JLabel("Status: ");
+    status =  new JTextField(15);
+    status.setEditable(false);
+     orderTotal = new JTextField(10);
+    orderTotal.setEditable(false);
+    
+    //output.setLayout( new FlowLayout(FlowLayout.TRAILING));
+    
+    
+    main.add(changeLabel);
+    main.add(change);
+    main.add(statusLabel);
+    main.add(status);
+
+}
+
+
+private class LemonListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            if (avLemonLime >0 ){
+            orderTotal.setText("$0.75");
+            avLemonLime = avLemonLime - 1;
+            inputMoney = Double.parseDouble(tendered.getText());
+            changeGiven = inputMoney - 0.75;
+            change.setText(String.valueOf(changeGiven));
+            }else{
+                status.setText("Sorry that item is out of stock!");
+            }   
+        }
     
 }
 
-private void buildOutputPanel(){
-    output = new JPanel();
-    JTextField change = new JTextField(10);
-    change.setEditable(false);
-    JTextField status =  new JTextField(10);
-    status.setEditable(false);
-    
-    
-    
-    output.add(change);
-    output.add(status);
+private class GrapeListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+               if (avGrape >0 ){
+            orderTotal.setText("$0.75");
+            avGrape = avGrape - 1;
+            inputMoney = Double.parseDouble(tendered.getText());
+            changeGiven = inputMoney - 0.75;
+            change.setText(String.valueOf(changeGiven));
+            }else{
+                status.setText("Sorry that item is out of stock!");
+                change.setText("0.00");
+            }
+        }
     
 }
+
+private class BWaterListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+                if (avBWater >0 ){
+            orderTotal.setText("$0.75");
+            avBWater = avBWater - 1;
+            inputMoney = Double.parseDouble(tendered.getText());
+            changeGiven = inputMoney - 0.75;
+            change.setText(String.valueOf(changeGiven));
+            }else{
+                status.setText("Sorry that item is out of stock!");
+                change.setText("0.00");
+            }
+        }
+    
+}
+
+private class ColaListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+                if (avCola >0 ){
+            orderTotal.setText("$0.75");
+            avCola = avCola - 1;
+            inputMoney = Double.parseDouble(tendered.getText());
+            changeGiven = inputMoney - 0.75;
+            change.setText(String.valueOf(changeGiven));
+            }else{
+                status.setText("Sorry that item is out of stock!");
+                change.setText("0.00");
+            }
+        }
+    
+}
+
+private class RBListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            if (avRb >0 ){
+            orderTotal.setText("$0.75");
+            avRb = avRb - 1;
+            inputMoney = Double.parseDouble(tendered.getText());
+            changeGiven = inputMoney - 0.75;
+            change.setText(String.valueOf(changeGiven));
+            }else{
+                status.setText("Sorry that item is out of stock!");
+                change.setText("0.00");
+            }
+            
+
+            
+            
+        }
+    
+}
+
 
 }
